fbef00f135a5b4476c182665ebc6fdb9b0505b70
Shuqing Tu <shuqing.tu@mail.utoronto.ca>
2024-10-27 18:20:40 -0400

    Update SquareTest1.java

--- SquareTest1.java
+++ SquareTest1.java
@@ -1,4 +1,4 @@
-package lab4;
+package Lab4;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -8,40 +8,41 @@ public class SquareTest {
 
     @Test
     public void testIsSquareWithValidSquare() {
-       
         Point A = new Point(0, 0);
         Point B = new Point(0, 2);
         Point C = new Point(2, 2);
         Point D = new Point(2, 0);
-        
         Square square = new Square(A, B, C, D);
-        
         assertTrue(square.isSquare(), "The points should form a square.");
     }
 
     @Test
     public void testIsSquareWithRectangle() {
-       
         Point A = new Point(0, 0);
         Point B = new Point(0, 3);
         Point C = new Point(2, 3);
         Point D = new Point(2, 0);
-        
         Square rectangle = new Square(A, B, C, D);
-        
         assertFalse(rectangle.isSquare(), "The points should not form a square.");
     }
 
     @Test
-    public void testIsSquareWithRandomPoints() {
-        
+    public void testIsSquareWithRandomShape() {
         Point A = new Point(0, 0);
         Point B = new Point(1, 3);
         Point C = new Point(4, 4);
         Point D = new Point(2, 1);
-        
         Square randomShape = new Square(A, B, C, D);
-        
         assertFalse(randomShape.isSquare(), "The points should not form a square.");
+    }
+
+    @Test
+    public void testIsSquareWithDegenerateCase() {
+        Point A = new Point(0, 0);
+        Point B = new Point(1, 1);
+        Point C = new Point(2, 2);
+        Point D = new Point(3, 3);
+        Square degenerate = new Square(A, B, C, D);
+        assertFalse(degenerate.isSquare(), "The points should not form a square.");
     }
 }
