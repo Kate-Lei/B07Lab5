f3fd2465ff9c7b74ee378246c3c4cfde405186db
Kate Lei <katelei@KatedeMacBook-Air.local>
2024-10-26 23:43:43 -0400

    Update Circle.java with new changes

--- Circle.java
+++ Circle.java
@@ -1,5 +1,32 @@
-public class Circle{
-	Point x;
-	
-	
+package Lab4;
+
+public class Circle {
+    private Point center;
+    private double radius; 
+    
+    public Circle(Point center, double radius) {
+        this.center = center;
+        this.radius = radius;
+    }
+    
+    public Point getCenter() {
+        return center;
+    }
+
+
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    
+    public double getCircumference() {
+        return 2 * Math.PI * radius;
+    }
+
+    public boolean contains(Point p) {
+        double distance = center.distance(p);
+        return distance <= radius;
+    }
+
+    
 }
