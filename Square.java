bc850e0f578d6bc724a0d6e7fd839860d4c65c86
Irene Yang <ireneyang@Irenes-MacBook-Pro.local>
2024-10-28 05:36:20 +0800

    Update isSquare method to handle arbitrary point order

--- Square.java
+++ Square.java
@@ -1,4 +1,6 @@
-package lab4;
+package Lab4;
+
+import java.util.Arrays;
 
 public class Square {
 	
@@ -15,9 +17,20 @@ public class Square {
 	}
 	
 	public boolean isSquare() {
-		return (A.distance(B) == B.distance(C) &&
-				B.distance(C) == C.distance(D) &&
-				C.distance(D) == D.distance(A));
+		double dAB = A.distance(B);
+	    double dAC = A.distance(C);
+	    double dAD = A.distance(D);
+	    double dBC = B.distance(C);
+	    double dBD = B.distance(D);
+	    double dCD = C.distance(D);
+	    
+	    double[] distances = {dAB, dAC, dAD, dBC, dBD, dCD};
+	    Arrays.sort(distances);
+	    
+	    return (distances[0] == distances[1] && distances[1] == distances[2] && distances[2] == distances[3]) &&
+	            (distances[4] == distances[5]) &&
+	            (distances[4] > distances[0]); 
+	    
 	}
 	
 	
